e7e51d646c6fc8b85a9e0a9649832b473f6c6351
Daniel Orduña <danordua@gmail.com>
2023-05-28 23:34:24 -0600

    Odyssey CS problem section updates

--- JS/index.js
+++ JS/index.js
@@ -5,22 +5,6 @@ const workContent = document.querySelector('.work-content');
 const odyssey = document.querySelector('.odyssey');
 const wbccares = document.querySelector('.wbccares');
 
-aboutButton.addEventListener('click', () =>{
-    aboutContent.style.display = "flex";
-    workContent.style.display = "none";
-
-    aboutButton.classList.add("selected-nav-i");
-    workButton.classList.remove("selected-nav-i");
-})
-
-workButton.addEventListener('click', () =>{
-    aboutContent.style.display = "none";
-    workContent.style.display = "flex";
-
-    aboutButton.classList.remove("selected-nav-i");
-    workButton.classList.add("selected-nav-i");
-})
-
 odyssey.addEventListener('click', () => {
     location.replace("odyssey.html")
 })
--- JS/odyssey.js
+++ JS/odyssey.js
@@ -1,5 +1,1 @@
 const backBtn = document.querySelector('.back');
-
-backBtn.addEventListener('click', () => {
-    location.replace("index.html")
-})
